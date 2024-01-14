7e16e175417cd380e13c57e88f509ce3803afb2a
Apriyanto28 <apriyanto.halim@mikroskil.ac.id>
2024-01-14 10:19:56 +0700

    Menambahkan komentar pada file build push

--- build_push_image.sh
+++ build_push_image.sh
@@ -1,7 +1,19 @@
+#kriteria: 3
+
+# Perintah dasar untuk file build
 #!/bin/bash
 
+# Membuat image docker dan diberikan nama item-app menggunakan versi 1
 docker build -t item-app:v1 .
+
+# Mendaftarkan setiap images yang terdapat di dalam docker lokal
 docker images
+
+# Menggantikan nama tag menjadi docker hub yang ditambahkan dengan username
 docker tag item-app:v1 craziyuhuu/item-app:v1
+
+# Menggunakan password yang sudah ada di dalam export dan melakukan login ke dalam docker hub
 echo $PASSWORD_DOCKER_HUB | docker login -u craziyuhuu --password-stdin
+
+# Melakukan publish terhadap image yang sudah dibuat di lokal ke dalam docker hub
 docker push craziyuhuu/item-app:v1
